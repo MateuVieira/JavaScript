47c5dc77e8c946706784e587e621e8564d773292
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-14 10:34:29 -0300

    Create removerPacientes.js

---
+++ Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/removerPacientes.js
@@ -0,0 +1,9 @@
+
+var pacientes = document.querySelectorAll(".pacientes");
+
+pacientes.forEach(function(paciente) {
+    paciente.addEventListener("dblclick", function(){
+
+        this.remove();
+    })
+});

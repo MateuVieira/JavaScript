8e0b04efc9781a281f26667b2c42ce0fbe40b1af
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-14 10:24:53 -0300

    Update calculaImc.js

--- Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/calculaImc.js
+++ Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/calculaImc.js
@@ -16,27 +16,40 @@ for(var i = 0; i < pacientes.length; i++){
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido =  validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
     
-        if( peso <= 0 || peso > 300){
+        if(!pesoEhValido){
             console.log("Peso inválido");
-            pesoEhValido = false;
             tdImc.textContent = "Peso inválido";
             paciente.classList.add("paciente-invalido");
         }
-    
-        if( altura <= 0 || altura > 3){
-            console.log("Atura inválido");
-            alturaEhValida = false;
-            tdImc.textContent = "Atura inválido";
+
+        if(!alturaEhValida){
+            console.log("Alturainválido");
+            tdImc.textContent = "Altura inválido";
             paciente.classList.add("paciente-invalido");
         }
+        
     
         if( pesoEhValido && alturaEhValida){
             tdImc.textContent = calculaImc(peso, altura);
         }
    
+}
+
+function validaAltura(altura){
+    if (altura <= 0 || altura > 3) {
+        return false;
+    }
+    return true;
+}
+
+function validaPeso(peso) {
+    if (peso <= 0 || peso > 300) { 
+        return false;
+    }
+    return true;
 }
 
 function calculaImc(peso, altura){

6b73bcc8d8de78b56031ae281696c49895d20da7
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-14 10:24:58 -0300

    Update form.js

--- Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/form.js
+++ Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/form.js
@@ -3,14 +3,21 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
-    
     var paciente = obtemPacienteDoFormulario(form);
-
     var pacienteTr = montaTr(paciente);
+    var erros = validaPaciente(paciente);
+
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+        
+        return;
+    }
 
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
     form.reset();
+    var mensagemErro = document.querySelector("#mensagem-erro");
+    mensagemErro.innerHTML = "";
 });
 
 function montaTr(paciente) {
@@ -45,4 +52,77 @@ function obtemPacienteDoFormulario(form) {
         imc: calculaImc(form.peso.value, form.altura.value)
     }
     return paciente;
+}
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if(!validaPeso(paciente.peso || validaPesoEmBranco(paciente.peso))){
+        erros.push("Peso inválido!");
+    }
+
+    if(!validaAltura(paciente.altura) || !validaAlturaEmBranco(paciente.altura)){
+        erros.push("Altura inválido!");
+    }
+
+    if(!validaGordura(paciente.gordura) || !validaGorduraEmBranco(paciente.gordura)){
+        erros.push("Campo gordura inválido!");
+    }
+
+    if(!validaNome(paciente.nome)){
+        erros.push("Nome inválido!");
+    }
+    
+
+    return erros;
+
+}
+
+function validaGordura(gordura){
+    if (gordura <= 0 || gordura > 60) {
+        return false;
+    }
+    return true;
+}
+
+function validaNome(nome){
+    if(nome.length == 0){
+        return false;
+    }
+    return true;
+}
+
+function validaPesoEmBranco(peso){
+    if(peso.length == 0){
+        return false;
+    }
+    return true;
+}
+
+function validaAlturaEmBranco(altura){
+    if(altura.length == 0){
+        return false;
+    }
+    return true;
+}
+
+function validaGorduraEmBranco(gordura){
+    if(gordura.length == 0){
+        return false;
+    }
+    return true;
+}
+
+function exibeMensagensDeErro(erros){
+
+    var ul = document.querySelector("#mensagem-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }

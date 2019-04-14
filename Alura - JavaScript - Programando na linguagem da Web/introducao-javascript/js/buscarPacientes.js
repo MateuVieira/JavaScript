f833537e36a462917539c241b38edb36bdab8ac8
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-14 14:37:03 -0300

    Update buscarPacientes.js

--- Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/buscarPacientes.js
+++ Alura - JavaScript - Programando na linguagem da Web/introducao-javascript/js/buscarPacientes.js
@@ -9,13 +9,18 @@ botaoBuscar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     
     xhr.addEventListener("load", function(){
-        var resposta = this.responseText;
 
-        var pacientes = JSON.parse(resposta);
+        if(xhr.status == 200){
+            var resposta = this.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(paciente => {
-            adicionaPacienteTabela(paciente);
-        });
+            pacientes.forEach(paciente => {
+                adicionaPacienteTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }    
     });
 
     xhr.send();

e65671c1417a1c08191e20d33ee7a958d80ca001
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-15 10:17:40 -0300

    Update ListaNegociacoes.js

--- Alura - javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ Alura - javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,22 @@
 
 class ListaNegociacoes{
 
-    constructor(){
+    constructor(acao){
         this._negociacoes = []
+        this._acao = acao;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
+        this._acao(this);
     }
 
     get negociacoes(){
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._acao(this);
     }
 }

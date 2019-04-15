6bcd3baa54d06cc7cf26a5dd28e0849501190f4b
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-14 22:44:39 -0300

    Update NegociacaoController.js

--- Alura - javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura - javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,10 +6,11 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     get data(){
-        return new DateHelper().textoParaDate(this._inputData.value);
+        return DateHelper.textoParaDate(this._inputData.value);
     }
 
     get quantidade(){
@@ -28,10 +29,18 @@ class NegociacaoController{
             this.quantidade,
             this.valor,
         );
-
-        let diaMesAno =  new DateHelper().dateParaTexto(negociacao.data);
-        console.log(diaMesAno);
         
+        this._listaNegociacoes.adiciona(negociacao);
+        
+        _limpaFormulario();
     }
-    
+
+    _limpaFormulario(){
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputQuantidade.focus();
+    }
 }

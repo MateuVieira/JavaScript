d5f1a3f66b48d37b1c8cce1db5b62fe5d0f73660
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-16 15:39:36 -0300

    Create boot.js

---
+++ Alura - javascript-avancado-i-aula1/aluraframe/client/js/app-es6/boot.js
@@ -0,0 +1,10 @@
+
+import {NegociacaoController} from './controllers/NegociacaoController';
+
+
+let negociacaoController = new NegociacaoController();
+
+document.querySelector('.form').onsubmit = negociacaoController.adiciona.bind(negociacaoController);
+document.querySelector('#buttonApaga').onsubmit = negociacaoController.apaga.bind(negociacaoController);
+
+

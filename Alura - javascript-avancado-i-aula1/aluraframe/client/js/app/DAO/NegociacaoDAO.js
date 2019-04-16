81f24bfe2c7b589679d6934a3237f133075e3528
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-15 22:52:53 -0300

    Create NegociacaoDAO.js

---
+++ Alura - javascript-avancado-i-aula1/aluraframe/client/js/app/DAO/NegociacaoDAO.js
@@ -0,0 +1,26 @@
+
+class NegociacaoDAO {
+
+    constructor(connection) {
+        
+        this._connection = connection;
+        this._store = 'negociacoes';
+    }
+
+    adiciona(negociacao) {
+
+        return new Promise((resolve,reject) => {
+
+            let request = this._connection
+                            .transaction([this._store], 'readwrite')
+                            .objctStore(this._store)
+                            add(negociacao);
+
+            request.onsuccess = e => resolve();
+        
+            request.onerror = e => {
+                reject('Não foi possível adicionar a negociação.')
+            }
+        });
+    }
+}

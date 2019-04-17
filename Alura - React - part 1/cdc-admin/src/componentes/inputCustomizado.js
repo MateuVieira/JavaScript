146369ed95635bc3df62521b12054850cb5de336
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-17 14:25:03 -0300

    Update inputCustomizado.js

--- Alura - React - part 1/cdc-admin/src/componentes/inputCustomizado.js
+++ Alura - React - part 1/cdc-admin/src/componentes/inputCustomizado.js
@@ -8,7 +8,7 @@ export default class InputCustumizado extends Component{
             <div className="pure-control-group">
                 <label htmlFor={this.props.id}>{this.props.label}</label> 
                 <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value}  onChange={this.props.onChange}/>                  
-            </div>
+            </div> 
         );
     }
 }

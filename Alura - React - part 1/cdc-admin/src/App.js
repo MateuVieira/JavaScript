852dd7b7d5a70d897c0f676e5f381cb949221a28
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-17 14:35:09 -0300

    Update App.js

--- Alura - React - part 1/cdc-admin/src/App.js
+++ Alura - React - part 1/cdc-admin/src/App.js
@@ -5,26 +5,6 @@ import {FormularioAutor, TabelaAutores} from './Autor/Autor';
 
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {lista : []};
-  }
-
-  componentDidMount() {
-
-    let url = `https://cdc-react.herokuapp.com/api/autores`;
-    
-    fetch(url, {
-      method: 'GET'
-    })
-    .then(res => res.json())
-    .then(data => data.filter(dado => dado.id < 100))
-    .then(data => this.setState({lista:data}))
-    .catch(error => console.error(error));
-    
-  }
-
   
   render() {
     return (
@@ -37,16 +17,17 @@ class App extends Component {
         </a>
 
         <div id="menu">
-            <div className="pure-menu">
-                <a className="pure-menu-heading" href="#">Company</a>
+          <div className="pure-menu">
+              <a className="pure-menu-heading" href="#">Company</a>
 
-                <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-                </ul>
-            </div>
+              <ul className="pure-menu-list">
+                  <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                  <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                  <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+              </ul>
+          </div>
         </div>
+       
 
         <div id="main">
             <div className="header">
@@ -55,28 +36,7 @@ class App extends Component {
             <br/>
             <div className="content" id="content">
               <FormularioAutor />
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      {
-                        this.state.lista.map(autor => {
-                          return (
-                            <tr key={autor.id}>
-                              <td>{autor.nome}</td>
-                              <td>{autor.email}</td>
-                            </tr>
-                          );
-                        })
-                      }               
-                  </tbody>
-                </table> 
-              </div>     
+              <TabelaAutores />
           </div>        
         </div>
       </div>

942cb66f2b693af031158bc378081a014c908476
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-17 14:24:54 -0300

    Update App.js

--- Alura - React - part 1/cdc-admin/src/App.js
+++ Alura - React - part 1/cdc-admin/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import InputCustomizado from './componentes/inputCustomizado';
-import InputSubmit from './componentes/inputSubmit';
-import {FormularioAutor, TabelaAutores} from './Autor';
+import {FormularioAutor, TabelaAutores} from './Autor/Autor';
 
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista : [], nome : '', email : '', senha : ''};
+    this.state = {lista : []};
   }
 
   componentDidMount() {
@@ -27,35 +25,7 @@ class App extends Component {
     
   }
 
-  enviaForm(evento) {
-
-    evento.preventeDefault();
-
-    let url = `https://cdc-react.herokuapp.com/api/autores`;
-
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({nome: this.state.nome , email:this.state.email , senha:this.state.senha}),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(res => console.log(res.status))
-    .catch(error => console.error(error));
-  }
-
-  setNome(evento) {
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento) {
-    this.setState({email:evento.target.value});
-  }
-
-  setSenha(evento) {
-    this.setState({senha:evento.target.value});
-  }
-
+  
   render() {
     return (
        
@@ -84,15 +54,7 @@ class App extends Component {
             </div>
             <br/>
             <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
-                  <InputCustomizado id="nome" type="text" label="Nome" name="nome" value={this.state.nome}  onChange={this.setNome.bind(this)}/>                  
-                  <InputCustomizado id="email" type="email" label="Email" name="email" value={this.state.email}  onChange={this.setEmail.bind(this)}/>                  
-                  <InputCustomizado id="senha" type="password" label="Senha" name="senha" value={this.state.senha}  onChange={this.setSenha.bind(this)}/>                  
-                  <InputSubmit type="submit" label="Gravar" />
-                </form>             
-
-              </div>  
+              <FormularioAutor />
               <div>            
                 <table className="pure-table">
                   <thead>

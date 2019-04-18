0e29b86f490b78e95dc1174be42736667f20e7b6
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-18 13:10:58 -0300

    Update Timeline.js

--- Alura - React - parte 2/instalura/src/componentes/Timeline.js
+++ Alura - React - parte 2/instalura/src/componentes/Timeline.js
@@ -3,12 +3,32 @@ import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
 
+    constructor() {
+        
+        super();
+        this.state = {listaFoto : []};
+    }
+
+    componentDidMount() {
+
+        let url = `https://instalura-api.herokuapp.com/api/public/fotos/rafael`;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(dados => this.setState({listaFoto:dados}))
+        .catch(error => console.error(error));
+        
+    }
+
     render() {
         return(
 
             <div className="fotos container">
-                <FotoItem/>
-                <FotoItem/>
+                {
+                    this.state.listaFoto.map(foto => <FotoItem foto={foto}/>)
+                }
+               {/*  <FotoItem/>
+                <FotoItem/> */}
             </div>   
 
         );

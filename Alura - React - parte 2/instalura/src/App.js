4fa01f93dfacb4cd102ef816dc6f3ea9146d9a7c
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-18 13:10:50 -0300

    Update App.js

--- Alura - React - parte 2/instalura/src/App.js
+++ Alura - React - parte 2/instalura/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
 
+
 class App extends Component {
   render() {
     return (
@@ -10,7 +11,7 @@ class App extends Component {
         <Header  />
         <Timeline  />
       </div>
-      
+
     );
   }
 }
